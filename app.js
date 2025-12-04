11ff9b5bdb3c8a67738b2e203f6b0f8268798009
ebytesolutionsec <ebyte.solutions.ec@gmail.com>
2025-12-04 00:01:22 -0500

    fix configuration

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+
+
+export default app;
